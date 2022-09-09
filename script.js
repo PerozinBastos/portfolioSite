968b41d0eba25660a52d99378b57f7903e5c5ef2
Guilherme Perozin <111192649+PerozinBastos@users.noreply.github.com>
2022-09-08 21:49:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let navbar = document.querySelector('.navbar');     /* css responsivo */
 
 menu.onclick = () => {
     navbar.classList.toggle('open-menu')
-    menu.classList.toggle('move');
+    menu.classList.toggle('move')
 };
 
 window.onscroll = () => {
     navbar.classList.remove('open-menu')
-    menu.classList.remove('move');
+    menu.classList.remove('move')
 };
 
 
@@ -68,5 +68,5 @@ window.addEventListener('scroll', () => {
 
     scrollTop.classList.toggle("scroll-active", window.scrollY >= 400);    // vai mudar a classe de .scroll-top para scroll-active do css ( não colocar . aqui)
      // scrollY recebe um valor //
-});
+})
 
